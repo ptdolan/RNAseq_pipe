0105f375529833ad1f60bd65b854bb95df72899a
Patrick Dolan <ptdolan@stanford.edu>
2018-08-03 09:36:50 -0700

    Delete environs.sh

--- environs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-module purge;
-module load biology;module load bowtie2;
-which bowtie2;
-module load tophat;
-which tophat;
-module load fastx_toolkit;
-module load viz; module load gnuplot;
-module list;
