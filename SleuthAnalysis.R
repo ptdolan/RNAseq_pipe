b2f144af983e5cbce4303b75f4c446e2c21e1ccc
PT Dolan <ptdolan@stanford.edu>
2018-08-03 15:19:10 -0700

    updated Sleuth script R

--- SleuthAnalysis.R
+++ SleuthAnalysis.R
@@ -16,6 +16,10 @@ installPKGS<-function(){
   install.packages(c("dplyr","reshape2","ggplot2","data.table","biomartr"))
 }
 
+annotation<-annotables::grcm38_tx2gene
+DF<-data.frame(annotation)
+colnames(DF)<-c("target_id","ensgene")
+
 library(dplyr)
 library(sleuth)
 library(reshape2)
@@ -26,29 +30,27 @@ library(biomartr)
 library(limma)
 
 #######################################################################
-
-kalDirRoot<-"/Users/ptdolan/Desktop/mNSC_KallistoAnalysis/KallistoOutputs/"
-
-#######################################################################
-#   BioMart
+#   BioMart - (un)comment depending on annotation type
 #######################################################################
 
-mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL",verbose = T,
-                         dataset = "mmusculus_gene_ensembl",
-                         host = "uswest.ensembl.org")
-
-bmIDs <- biomaRt::getBM(
-  attributes = c("ensembl_gene_name","refseq_mrna_predicted","refseq_mrna"),
-  mart = mart)
+# mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL",verbose = T,
+#                          dataset = "mmusculus_gene_ensembl",
+#                          host = "uswest.ensembl.org")
+# 
+# bmIDs <- biomaRt::getBM(
+#   attributes = c("ensembl_gene_name","external_gene_name","refseq_mrna"),
+#   mart = mart)
 
 #######################################################################
 #   SLEUTH
 #######################################################################
 
+kalDirRoot<-"/Users/ptdolan/Desktop/ENSout/"
+
 kalDirs=list.dirs(kalDirRoot,full.names = T)
 
-age=strsplit2(kalDirs,"_")[-c(1,14),4]
-exp=strsplit2(kalDirs,"_")[-c(1,14),5]
+age=strsplit2(kalDirs,"_")[-c(1,14),3]
+exp=strsplit2(kalDirs,"_")[-c(1,14),4]
 diff<-strsplit2(exp,"-")[,2]
 
 cond<-paste(age,diff)
@@ -59,7 +61,7 @@ input<-data.table(sample=paste(age,exp,sep = "-"),
                   path=kalDirs[c(-1,-14)])
 
 # Initialize sleuth object
-SO<-sleuth_prep(input,read_bootstrap_tpm=T,extra_bootstrap_summary=T)
+SO<-sleuth_prep(input,target_mapping = DF,aggregation_column = "ensgene", read_bootstrap_tpm=T,extra_bootstrap_summary=T)
 
 # Fit models
 SO <- sleuth_fit(SO, ~age+diff, 'full' )
@@ -77,12 +79,14 @@ SO<- sleuth_wt(SO,'diffD')
 
 #Output tables
 Diff_sleuth_table <- sleuth_results(SO, 'age:full', 'lrt', show_all = F)
-Diff_sleuth_table$refseq_gene<-strsplit2(Diff_sleuth_table$target_id,split = "\\.")[,1]
-Diff_sleuth_table<-unique(merge.data.frame(Diff_sleuth_table,mapping,by="refseq_gene"))
 Age_sleuth_table <- sleuth_results(SO, 'diff:full', 'lrt', show_all = FALSE)
+
+Diff_sleuth_table<-merge(Diff_sleuth_table,annotables::grcm38,by.x = "target_id",by.y="ensgene")
+Age_sleuth_table<-merge(Age_sleuth_table,annotables::grcm38,by.x = "target_id",by.y="ensgene")
+
 AgeDiff_sleuth_table <- sleuth_results(SO, 'null:full', 'lrt', show_all = FALSE)
 Wald_Diff_sleuth_table <- sleuth_results(SO,test = "diffD",test_type ='wt', show_all = F)
-
+cyto
 Diff_sleuth_significant <- dplyr::filter(Diff_sleuth_table, qval <= 0.05)
 Age_sleuth_significant <- dplyr::filter(Age_sleuth_table, qval <= 0.05)
 AgeDiff_sleuth_significant <- dplyr::filter(AgeDiff_sleuth_table, qval <= 0.05)
@@ -93,13 +97,10 @@ write.csv(AgeDiff_sleuth_significant,file = "mNSC_AgeDiff_Sig-q05.csv")
 ########################################################################
 # PHN analysis 
 ########################################################################
-sleuth_save(SO,"~/GitHub/mNSC_differentiation.sleuth")
-
-sleuth_live(SO)
 
 mergedData<-merge(SO$obs_raw,SO$sample_to_covariates,by="sample")
 mergedData$refseq_gene<-strsplit2(mergedData$target_id,split = "\\.")[,1]
 mergedData<-unique(merge.data.frame(mergedData,mapping,by="refseq_gene"))
 
-ggplot(mergedData[])+geom_boxplot(aes(diff,tpm))
+ggplot(mergedData)+geom_boxplot(aes(diff,tpm))
 
